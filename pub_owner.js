57f43edb051f8a57da070339ebeed471c8334bbf
mariannedejong <89845904+mariannedejong@users.noreply.github.com>
2021-11-09 16:17:37 +0100

    Add files via upload

---
+++ pub_owner.js
@@ -0,0 +1,14 @@
+const Age = 17;
+const isFemale = true;
+const driverStatus = 'bob';
+if (Age>=18){
+    console.log("You are welcome to come in :) ");
+} else {console.log ("Sorry, you are too young to enter our pub :(")}
+
+if (isFemale){
+    console.log("Tickets for our famous ladies night are now for sale!");
+} else {console.log("Sorry, ladies night is for women only");}
+
+if (driverStatus == 'bob'){
+    console.log("Your driver status is bob, so you are allowed to drive");
+} else {console.log("You don't have the driver status bob, so you are not allowed to drive");}
